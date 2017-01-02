59c5bb6e180826d90386714ba9c228b19201a975
Dong Kim <dan_kim93@hotmail.com>
2017-01-02 11:59:10 -0800

    first commit phase 1

---
+++ ReduxTodos/frontend/todo_redux.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("hi");
+  ReactDOM.render(<h1>Todos</h1>, document.getElementById('root'));
+});
